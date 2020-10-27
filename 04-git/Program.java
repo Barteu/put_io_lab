341051b8e1ca96d49750c6d78d85ec61a8c25eb5
Bartek Szymkowiak <szymk.b9@gmail.com>
2020-10-27 20:19:31 +0100

    Pierwsza zmiana

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,4 +1,4 @@
-// comment 1
+// komentarz w pierwszej linijce
 public class MyClass {
 	int x = 25;
 	
@@ -7,6 +7,7 @@ public class MyClass {
     System.out.println("-----");
     System.out.println(myObj.x);
     System.out.println("::-)");
+    //loop
     for(int i=0;i<4;i++)
     {
 	 System.out.println(i);
