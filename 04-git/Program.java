cc0a15b1b43345d30861c7598c26b6c5b5d29b99
Bartek Szymkowiak <szymk.b9@gmail.com>
2020-10-27 20:13:03 +0100

    PRZYGOTOWANIE DO ZROBENIA ZAD6 NA NOWO

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,16 +1,12 @@
 // comment 1
-// comment 2
 public class MyClass {
-  	// variable x
 	int x = 25;
 	
   public static void main(String[] args) {
     MyClass myObj = new MyClass();
-    //prints
     System.out.println("-----");
     System.out.println(myObj.x);
     System.out.println("::-)");
-    // interesting loop
     for(int i=0;i<4;i++)
     {
 	 System.out.println(i);
